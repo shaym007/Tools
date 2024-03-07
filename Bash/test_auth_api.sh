90f5a6b92631f98bdc5e61888277ac56dfc8cd2c
Shay Meshulam <shaymeshulam@Shays-MacBook-Pro.local>
2024-03-07 12:24:19 +0200

    adding test API Auth script

---
+++ Bash/test_auth_api.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+###################
+# The scripts runs API request to k8s auth via Akeyless GW (API, port 8080)
+####################
+
+echo -e "Please provide the folllwing:\nAccess-ID:"
+read ACCESS_ID
+
+echo -e "\nGW URL with port and path (e.g http://<gw-url>:8080/v2/auth)"
+read GW_URL
+
+echo -e "\nAuth config name:"
+read AUTH_CONFIG_NAME
+
+# Get the token from the GW with
+# kubectl exec -it gw-akeyless-api-gateway-98cd8d467-pscbc -- /bin/bash -c "cat /var/run/secrets/kubernetes.io/serviceaccount/token | base64 -w 0; echo "
+
+echo -e "\nk8s service account token (cat cat /var/run/secrets/kubernetes.io/serviceaccount/token | base64 -w0):"
+read TOKEN
+
+
+
+generate_post_data () {
+
+cat << EOF
+{
+  "access-type": "k8s",
+  "json": false,
+  "debug": true,
+  "access-id": "$ACCESS_ID",
+  "k8s-auth-config-name": "$AUTH_CONFIG_NAME",
+  "k8s-service-account-token": "$TOKEN"
+}
+EOF
+}
+
+curl --request POST \
+     --url $GW_URL \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data "$(generate_post_data)"
